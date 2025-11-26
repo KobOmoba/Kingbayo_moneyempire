f2b021dde5361deb7f58dfe5af633fc5a9f97624
KobOmoba <aadesanya173@gmail.com>
2025-11-26 13:50:59 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,34 +1,6 @@
 export default {
   plugins: {
-    '@tailwindcss/postcss': {
-      config: './tailwind.config.js'
-    },
-    autoprefixer: {
-      overrideBrowserslist: [
-        '>1%',
-        'last 4 versions',
-        'Firefox ESR',
-        'not ie < 9'
-      ],
-      flexbox: 'no-2009',
-      grid: 'autoplace'
-    },
-    'postcss-preset-env': {
-      stage: 3,
-      features: {
-        'nesting-rules': true,
-        'custom-media-queries': true,
-        'media-query-ranges': true
-      }
-    },
-    'cssnano': process.env.NODE_ENV === 'production' ? {
-      preset: ['default', {
-        discardComments: {
-          removeAll: true
-        },
-        normalizeWhitespace: true,
-        colormin: true
-      }]
-    } : false
+    tailwindcss: {},
+    autoprefixer: {},
   }
 }
